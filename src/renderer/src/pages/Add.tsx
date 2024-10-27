1642867e559379ccd831f635d8ffd30b9debf41a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-10-27 18:23:34 +0900

    add label

--- src/renderer/src/pages/Add.tsx
+++ src/renderer/src/pages/Add.tsx
@@ -46,50 +46,66 @@ const Add: React.FC<AddProps> = ({ investments, setInvestments }) => {
     <div className="container">
       <h2 className="my-4">Add New Investment</h2>
       <div className="mb-3">
-        <input
-          type="text"
-          name="name"
-          placeholder="Stock name"
-          value={newInvestment.name}
-          onChange={handleChange}
-          className="form-control mb-2"
-        />
-        <input
-          type="number"
-          name="purchasePrice"
-          placeholder="Purchase Price"
-          value={newInvestment.purchasePrice}
-          onChange={handleChange}
-          className="form-control mb-2"
-        />
-        <input
-          type="number"
-          name="currentPrice"
-          placeholder="Current Price"
-          value={newInvestment.currentPrice}
-          onChange={handleChange}
-          className="form-control mb-2"
-        />
-        <input
-          type="number"
-          name="amount"
-          placeholder="Quantity"
-          value={newInvestment.amount}
-          onChange={handleChange}
-          className="form-control mb-2"
-        />
-        <input
-          type="date"
-          name="purchaseDate"
-          value={newInvestment.purchaseDate.toISOString().substring(0, 10)}
-          onChange={(e) =>
-            setNewInvestment((prev) => ({
-              ...prev,
-              purchaseDate: new Date(e.target.value)
-            }))
-          }
-          className="form-control mb-2"
-        />
+        <div className="mb-2">
+          <label htmlFor="name">Stock Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={newInvestment.name}
+            onChange={handleChange}
+            className="form-control mb-2"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="purchasePrice">Purchase Price</label>
+          <input
+            type="number"
+            id="purchasePrice"
+            name="purchasePrice"
+            value={newInvestment.purchasePrice}
+            onChange={handleChange}
+            className="form-control mb-2"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="currentPrice">Current Price</label>
+          <input
+            type="number"
+            id="currentPrice"
+            name="currentPrice"
+            value={newInvestment.currentPrice}
+            onChange={handleChange}
+            className="form-control mb-2"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="amount">Quantity</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={newInvestment.amount}
+            onChange={handleChange}
+            className="form-control mb-2"
+          />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="purchaseDate">Purchase Date</label>
+          <input
+            type="date"
+            id="purchaseDate"
+            name="purchaseDate"
+            value={newInvestment.purchaseDate.toISOString().substring(0, 10)}
+            onChange={(e) =>
+              setNewInvestment((prev) => ({
+                ...prev,
+                purchaseDate: new Date(e.target.value)
+              }))
+            }
+            className="form-control mb-2"
+          />
+        </div>
         <button onClick={handleAddInvestment} className="btn btn-primary">
           Add
         </button>
